576e2a9f9d72d6e8d6574043c41e464bae0eba6b
SalHe <SalHe@qq.com>
2021-02-25 12:10:55 +0800

    添加项目文件。

---
+++ Period/Week01/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 我把个人相关信息单独放到一个文件里了
+            // 然后就不需要将此文件上传至Git仓库了
+            XmlDocument doc = new XmlDocument();
+            doc.Load("MyInfo.xml");
+            string name = doc["MyInfo"]["Name"].InnerText;
+
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
