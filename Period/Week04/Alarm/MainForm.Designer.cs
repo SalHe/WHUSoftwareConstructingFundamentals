6a5254616a96326f77e31e6adde357ff6b63a1a1
SalHe <SalHe@qq.com>
2021-03-18 17:53:41 +0800

    第4次

---
+++ Period/Week04/Alarm/MainForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Alarm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cbTick = new System.Windows.Forms.CheckBox();
+            this.cbAlarm = new System.Windows.Forms.CheckBox();
+            this.tbTick = new System.Windows.Forms.TextBox();
+            this.labelAlarm = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            // 
+            // cbTick
+            // 
+            this.cbTick.AutoSize = true;
+            this.cbTick.Location = new System.Drawing.Point(15, 235);
+            this.cbTick.Name = "cbTick";
+            this.cbTick.Size = new System.Drawing.Size(90, 24);
+            this.cbTick.TabIndex = 0;
+            this.cbTick.Text = "启用Tick";
+            this.cbTick.UseVisualStyleBackColor = true;
+            this.cbTick.CheckedChanged += new System.EventHandler(this.checkBoxToEnableEvent_CheckedChanged);
+            // 
+            // cbAlarm
+            // 
+            this.cbAlarm.AutoSize = true;
+            this.cbAlarm.Location = new System.Drawing.Point(15, 269);
+            this.cbAlarm.Name = "cbAlarm";
+            this.cbAlarm.Size = new System.Drawing.Size(104, 24);
+            this.cbAlarm.TabIndex = 1;
+            this.cbAlarm.Text = "启用Alarm";
+            this.cbAlarm.UseVisualStyleBackColor = true;
+            this.cbAlarm.CheckedChanged += new System.EventHandler(this.checkBoxToEnableEvent_CheckedChanged);
+            // 
+            // tbTick
+            // 
+            this.tbTick.Location = new System.Drawing.Point(121, 235);
+            this.tbTick.Name = "tbTick";
+            this.tbTick.ReadOnly = true;
+            this.tbTick.Size = new System.Drawing.Size(249, 27);
+            this.tbTick.TabIndex = 2;
+            // 
+            // labelAlarm
+            // 
+            this.labelAlarm.AutoSize = true;
+            this.labelAlarm.Location = new System.Drawing.Point(121, 269);
+            this.labelAlarm.Name = "labelAlarm";
+            this.labelAlarm.Size = new System.Drawing.Size(0, 20);
+            this.labelAlarm.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 13);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(359, 216);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(353, 202);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "题目：\r\n使用事件机制，模拟实现一个闹钟功能。闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。在闹钟走时时或者响铃时，在控制台/Winform界面上显" +
+    "示提示信息。\r\n\r\n说明：\r\n当启用Tick时，将在TextBox内显示时间，并每秒发出鸣叫声；当启用Alarm时，将在Label内显示时间，并会在指定的时间弹" +
+    "出提示。";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(121, 299);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(250, 27);
+            this.dateTimePicker1.TabIndex = 5;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 336);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.labelAlarm);
+            this.Controls.Add(this.tbTick);
+            this.Controls.Add(this.cbAlarm);
+            this.Controls.Add(this.cbTick);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "⏰闹钟";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox cbTick;
+        private System.Windows.Forms.CheckBox cbAlarm;
+        private System.Windows.Forms.TextBox tbTick;
+        private System.Windows.Forms.Label labelAlarm;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ Period/Week04/Alarm/MainForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DateTime = System.DateTime;
+
+namespace Alarm
+{
+    public partial class MainForm : Form
+    {
+
+        private Dictionary<CheckBox, EventHandler> eventHandlers;
+
+        private KeyValuePair<int, int> alarmTime;
+        private bool alarmed = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // 不同的 CheckBox 对应了其相应的 Event
+            // 我们在此处为他们的关系建立字典
+            // 方便后面使用
+            eventHandlers = new Dictionary<CheckBox, EventHandler>
+            {
+                [cbTick] = eventTick,
+                [cbAlarm] = eventAlarm
+            };
+
+            // 让所有的 CheckBox 默认都选中
+            foreach (var entry in eventHandlers)
+            {
+                entry.Key.Checked = true;
+            }
+
+            // 让闹钟时间为下一分钟
+            dateTimePicker1.Value = DateTime.Now + TimeSpan.FromMinutes(1);
+
+            // 调整 Picker 的时间格式
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "HH:mm";
+        }
+
+        private void checkBoxToEnableEvent_CheckedChanged(object sender, EventArgs e)
+        {
+            // 两个CheckBox的CheckedChanged事件都被此函数注册了
+            // 所以下面会根据 sender 的不同，注册/取消注册相应的事件
+            if (sender is CheckBox c)
+            {
+                if (c.Checked)
+                    timer1.Tick += eventHandlers[c];
+                else
+                    timer1.Tick -= eventHandlers[c];
+            }
+        }
+
+        private void eventTick(object sender, EventArgs e)
+        {
+            tbTick.Text = "Tick" + DateTime.Now.ToLongTimeString();
+            Console.Beep();
+        }
+
+        private void eventAlarm(object sender, EventArgs e)
+        {
+            var now = DateTime.Now;
+            labelAlarm.Text = "Alarm" + now.ToLongTimeString();
+            if (now.Hour == alarmTime.Key && now.Minute == alarmTime.Value && !alarmed)
+            {
+                alarmed = true;
+                MessageBox.Show($"您{alarmTime.Key:D2}:{alarmTime.Value:D2}的闹钟已经到了哦~~");
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            alarmTime = new KeyValuePair<int, int>(dateTimePicker1.Value.Hour, dateTimePicker1.Value.Minute);
+            alarmed = false;
+        }
+    }
+}
