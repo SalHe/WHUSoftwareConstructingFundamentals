80ce94813f12ece193df29ec0a8d3b617e44ef4e
SalHe <SalHe@qq.com>
2021-04-08 16:38:26 +0800

    hw07 CayleyTree.csproj

---
+++ Period/Week07/CayleyTree/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace CayleyTree
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbTh2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbTh1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbPer2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbPer1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbLength = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbDepth = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.labelColor);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.tbTh2);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.tbTh1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.tbPer2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.tbPer1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbLength);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbDepth);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(310, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(310, 304);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "参数控制";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 30);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelColor
+            // 
+            this.labelColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelColor.Location = new System.Drawing.Point(128, 232);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(176, 26);
+            this.labelColor.TabIndex = 15;
+            this.labelColor.Click += new System.EventHandler(this.labelColor_Click);
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(14, 234);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 25);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "画笔颜色:";
+            // 
+            // tbTh2
+            // 
+            this.tbTh2.Location = new System.Drawing.Point(130, 196);
+            this.tbTh2.Name = "tbTh2";
+            this.tbTh2.Size = new System.Drawing.Size(175, 25);
+            this.tbTh2.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(14, 199);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "右分支角度:";
+            // 
+            // tbTh1
+            // 
+            this.tbTh1.Location = new System.Drawing.Point(130, 161);
+            this.tbTh1.Name = "tbTh1";
+            this.tbTh1.Size = new System.Drawing.Size(175, 25);
+            this.tbTh1.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(14, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "左分支角度:";
+            // 
+            // tbPer2
+            // 
+            this.tbPer2.Location = new System.Drawing.Point(130, 126);
+            this.tbPer2.Name = "tbPer2";
+            this.tbPer2.Size = new System.Drawing.Size(175, 25);
+            this.tbPer2.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(14, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "右分支长度比:";
+            // 
+            // tbPer1
+            // 
+            this.tbPer1.Location = new System.Drawing.Point(130, 92);
+            this.tbPer1.Name = "tbPer1";
+            this.tbPer1.Size = new System.Drawing.Size(175, 25);
+            this.tbPer1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(14, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "左分支长度比:";
+            // 
+            // tbLength
+            // 
+            this.tbLength.Location = new System.Drawing.Point(130, 57);
+            this.tbLength.Name = "tbLength";
+            this.tbLength.Size = new System.Drawing.Size(175, 25);
+            this.tbLength.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(14, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "主干长度:";
+            // 
+            // tbDepth
+            // 
+            this.tbDepth.Location = new System.Drawing.Point(130, 26);
+            this.tbDepth.Name = "tbDepth";
+            this.tbDepth.Size = new System.Drawing.Size(175, 25);
+            this.tbDepth.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(14, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "递归深度:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 326);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelColor;
+
+        private System.Windows.Forms.Label label7;
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbPer2;
+        private System.Windows.Forms.TextBox tbTh1;
+        private System.Windows.Forms.TextBox tbTh2;
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbPer1;
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbDepth;
+        private System.Windows.Forms.TextBox tbLength;
+
+        #endregion
+    }
+}
---
+++ Period/Week07/CayleyTree/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CayleyTree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            labelColor.BackColor = Color.Blue;
+
+            tbTh1.Text = th1.ToString();
+            tbTh2.Text = th2.ToString();
+            tbPer1.Text = per1.ToString();
+            tbPer2.Text = per2.ToString();
+            tbDepth.Text = 10.ToString();
+            tbLength.Text = "100";
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            graphics ??= this.CreateGraphics();
+            try
+            {
+                th1 = double.Parse(tbTh1.Text);
+                th2 = double.Parse(tbTh2.Text);
+                per1 = double.Parse(tbPer1.Text);
+                per2 = double.Parse(tbPer2.Text);
+                graphics.Clear(Color.White);
+                button1.Enabled = false;
+                drawCayleyTree(int.Parse(tbDepth.Text), 200, 310, int.Parse(tbLength.Text), -Math.PI / 2);
+            }
+            catch (FormatException exception)
+            {
+                MessageBox.Show("请输入正确的数字格式!");
+            }
+            finally
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private Graphics graphics;
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 20 * Math.PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.7;
+
+        void drawCayleyTree(int n,
+            double x0, double y0, double leng, double th)
+        {
+            if (n <= 0) return;
+
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+
+            drawLine(x0, y0, x1, y1);
+
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x1, y1, per2 * leng, th - th2);
+        }
+
+        void drawLine(double x0, double y0, double x1, double y1)
+        {
+            graphics.DrawLine(
+                new Pen(labelColor.BackColor),
+                (int) x0, (int) y0, (int) x1, (int) y1);
+        }
+
+        private ColorDialog _colorDialog;
+
+        private void labelColor_Click(object sender, EventArgs e)
+        {
+            _colorDialog ??= new ColorDialog()
+            {
+                Color =  labelColor.BackColor
+            };
+            if (_colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                labelColor.BackColor = _colorDialog.Color;
+            }
+        }
+    }
+}
