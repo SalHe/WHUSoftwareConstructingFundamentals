cf6e367d8e6a4622d3f1a00de03ea4d4ad07d323
SalHe <SalHe@qq.com>
2021-04-23 16:28:24 +0800

    add 身份证校验码

--- Period/Week09/IDNumberRegex/MainForm.cs
+++ Period/Week09/IDNumberRegex/MainForm.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -23,15 +25,40 @@ namespace IDNumberRegex
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+
+                if (CheckIDNumber(textBox1.Text))
+                {
+                    MessageBox.Show("验证成功！", "验证", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "验证", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool CheckIDNumber(string ID)
+        {
             if (validatorRegex.IsMatch(textBox1.Text))
             {
-                MessageBox.Show("验证成功！", "验证", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                int sum = 0;
+                for (int i = 0; i < 17; i++)
+                {
+                    int a = ID[i] - '0';
+                    int w = (int)Math.Pow(2, 17 - i) % 11;
+                    Debug.WriteLine($"{a} * {w}");
+                    sum += a * w;
+                }
+                int final = 12 - sum % 11;
+                char last = (char)(final < 10 ? ('0' + final) : 'x');
+                if (char.ToLower(ID[17]) == last)
+                    return true;
+                else
+                    throw new ArgumentException("校验码有误！");
             }
-            else
-            {
-                MessageBox.Show("验证失败！", "验证", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-
+            throw new ArgumentException("身份证格式错误！");
         }
     }
 }
